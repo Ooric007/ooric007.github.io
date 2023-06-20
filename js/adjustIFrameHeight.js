2ba7c2d3b94cd94fc4e6ce56e21d51c64d7bee4b
Eric Waldbaum <Ooric@Erics-Slim-iMac.hsd1.wa.comcast.net>
2023-06-20 01:49:36 -0700

    Made sections for shared code, updated main background, and added Amazon and Microsoft pages

---
+++ js/adjustIFrameHeight.js
@@ -0,0 +1,33 @@
+function resizeIframe(iframe) {
+    if (window.innerWidth <= 768) {
+        if (iframe === document.querySelector("#iframe-container iframe:nth-child(2)")) {
+            // Adjust the height for the second iframe on mobile
+            iframe.style.height = "88px"; // Set the desired height for the second iframe on mobile
+          } else {
+            // Adjust the height for other iframes on mobile
+            iframe.style.height = "584px"; // Set the desired height for mobile iframes other than the second one
+          }
+    } else if (iframe === document.querySelector("#iframe-container iframe:last-child")) {
+      // Adjust the height for the last iframe on desktop
+      iframe.style.height = "74px"; // Set the desired height for the last iframe
+    } else {
+      // Resize other iframes as before on desktop
+      iframe.style.height = iframe.contentWindow.document.documentElement.scrollHeight + "px";
+    }
+  }
+  
+  function adjustIframeHeight() {
+    var iframes = document.querySelectorAll("#iframe-container iframe");
+    iframes.forEach(function(iframe) {
+      resizeIframe(iframe);
+    });
+  }
+  
+  window.addEventListener("DOMContentLoaded", function() {
+    adjustIframeHeight();
+  });
+  
+  window.addEventListener("resize", function() {
+    adjustIframeHeight();
+  });
+  
--- js/breakline.js
+++ js/breakline.js
@@ -1,14 +1,18 @@
-window.addEventListener('resize', adjustLineBreak);
-
 function adjustLineBreak() {
   const byBreakElement = document.querySelector('.by-break');
   const isMobile = window.innerWidth <= 768;
   
-  if (isMobile) {
-    byBreakElement.classList.add('by-break-line-break');
-  } else {
-    byBreakElement.classList.remove('by-break-line-break');
+  if (byBreakElement) {
+    if (isMobile) {
+      byBreakElement.classList.add('by-break-line-break');
+    } else {
+      byBreakElement.classList.remove('by-break-line-break');
+    }
   }
 }
 
-adjustLineBreak();
+document.addEventListener('DOMContentLoaded', function() {
+  adjustLineBreak();
+});
+
+window.addEventListener('resize', adjustLineBreak);
